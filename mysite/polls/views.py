2f7c11926993900b655bc4f56a74055b92ad3d10
windycon <lfy867233516@outlook.com>
2018-03-12 00:17:11 +0800

    django official part 5 and change some past

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,42 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from django.shortcuts import get_object_or_404,render
-from django.view import generic
+from django.views import generic
 
 from .models import Question, Choice
 
-# Create your views here.
-#def index(request):
-#	lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-#	context = {'lastest_question_list': lastest_question_list}
-#	return render(request, 'polls/index.html', context)
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'lastest_question_list'
 	
 	def get_queryset(self):
-		return Question.objects.order_by("-pub_date")[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
-#def detail(request, question_id):
-	#try:
-	#	question = Question.objects.get(pk=question_id)
-	#except Question.DoesNotExist:
-	#	raise Http404("Question does not exist")
-	#question = get_object_or_404(Question, pk=question_id)
-	#return render(request, 'polls/detail.html', {'question': question})
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
-#def results(request, question_id):
-#	question = get_object_or_404(Question, pk=question_id)
-#	return render(request, 'polls/results.html', {'question': question})
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
 	
-# 处理请求
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
